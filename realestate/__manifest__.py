259d157e6ca8ddf11fd9f7ef2a3bad6e4cd42cab
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-17 14:52:21 +0430

    inhert user form and add user properties page to form

--- realestate/__manifest__.py
+++ realestate/__manifest__.py
@@ -29,6 +29,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/res_user_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
