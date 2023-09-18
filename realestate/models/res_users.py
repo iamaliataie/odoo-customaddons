d39e0e09f24e52ddaef5d90af90936642d7954d0
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-18 09:55:09 +0430

    add domain to property_ids in inherited ResUser model

--- realestate/models/res_users.py
+++ realestate/models/res_users.py
@@ -3,4 +3,4 @@ from odoo import models, fields, api
 class ResUser(models.Model):
     _inherit ='res.users'
 
-    property_ids = fields.One2many('realestate.property', 'salesman', string='Properties')
+    property_ids = fields.One2many('realestate.property', 'salesman', string='Properties', domain=[('state', 'in', ['new', 'offer_received'])])
