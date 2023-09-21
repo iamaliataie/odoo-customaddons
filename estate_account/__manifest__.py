ccf328fdc2a7d26e2ac016a8e22df8053875ddc6
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-21 09:57:48 +0430

    update manifest for estate_account module

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "estate_account",
+    'name': "Estate Account",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Estate Account application to handle invoicing process for real estate application""",
 
     'description': """
-        Long description of module's purpose
+        Estate Account application to handle invoicing process for real estate application
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Ali Ahmad Ataie",
+    'website': "https://www.ataie.dev",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Accounting',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
