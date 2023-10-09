adc79c57b878aeafa158c3c2fb7bbb7b45849f6b
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-10-09 13:57:46 +0430

    read data from todo.js in todo.xml

--- owl_todo/static/src/components/todo/todo.js
+++ owl_todo/static/src/components/todo/todo.js
@@ -6,7 +6,28 @@ const { Component, useState } = owl;
 export class OwlTodoList extends Component {
     setup(){
         this.state = useState(
-            {value: 1}
+            {
+                taskList: [
+                    {
+                        id: 1,
+                        name: 'Task 1',
+                        completed: true,
+                        color: '#ff1155'
+                    },
+                    {
+                        id: 2,
+                        name: 'Task 2',
+                        completed: true,
+                        color: '#55ff11'
+                    },
+                    {
+                        id: 3,
+                        name: 'Task 3',
+                        completed: true,
+                        color: '#1155ff'
+                    },
+                ],
+            }
         )
     }
 }
