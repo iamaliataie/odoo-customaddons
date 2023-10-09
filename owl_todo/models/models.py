07e4c4509c4848d0247285e50080bc04ad6837b6
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-10-09 09:43:46 +0430

    define OwlTodo model in owl_todo module

--- owl_todo/models/models.py
+++ owl_todo/models/models.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class owl_todo(models.Model):
-#     _name = 'owl_todo.owl_todo'
-#     _description = 'owl_todo.owl_todo'
+class OwlTodo(models.Model):
+    _name = 'owl.todo.list'
+    _description = 'Owl Todo List Model'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(string='Task Name')
+    color = fields.Char()
+    completed = fields.Boolean()
+
